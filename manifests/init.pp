26dcb99668003aec38acbbb0374af6e427530123
jayabalan <jayabalan.b@fireeye.com>
2018-07-10 11:13:52 +0000

    Added systemd configs to vault and service manage parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,16 @@ class vault (
   $group                               = $::vault::params::group,
   $manage_group                        = $::vault::params::manage_group,
   $download_url                        = $::vault::params::download_url,
+  $purge_config_dir		       = true,
 
 ) inherits ::vault::params {
 
   contain ::vault::install
+  contain ::vault::config
+  contain ::vault::service
 
+  Class['vault::install'] -> Class['vault::config']
+  Class['vault::config'] ~> Class['vault::service']
 
 }
 
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,40 +1,33 @@
 class vault::install {
-
-      $vault_bin  = "${::vault::bin_dir}/vault"
-
-    
-    
-  
-    archive { "${::vault::download_dir}/vault.zip":
-          ensure       => present,
-          extract      => true,
-          extract_path => $::vault::bin_dir,
-          source       => $::vault::download_url,
-          cleanup      => true,
-          creates      => $vault_bin,
-          before       => File['vault_bin'],
-      }
-
-    file { $vault_bin:
-    	owner => 'root',
-    	group => 'root',
-    	mode  => '0555',
-      }
-
-
-      file { '/etc/systemd/system/vault.service':
-          ensure  => file,
-          owner   => 'root',
-          group   => 'root',
-          mode    => '0644',
-          content => template('vault/vault.systemd.erb'),
-          notify  => Exec['systemd-reload'],
-        } 
-      exec {'systemd-reload':
-            command     => 'systemctl daemon-reload',
-            path        => '/bin:/usr/bin:/sbin:/usr/sbin',
-            user        => 'root',
-            refreshonly => true,
-          }
-
+  $vault_bin  = "${::vault::bin_dir}/vault"
+  package {'unzip':
+    provider =>  'yum',
+    ensure   =>  installed,    
+  }
+  file { $::vault::download_dir:
+    ensure   =>  directory,
+  }  
+  archive { "${::vault::download_dir}/vault.zip":
+    ensure       => present,
+    extract      => true,
+    extract_path => $::vault::bin_dir,
+    source       => $::vault::download_url,
+    cleanup      => true,
+    creates      => $vault_bin,
+    before       => File['vault_binary'],
+  }
+  file { 'vault_binary':
+    path  =>  $vault_bin,
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+  }
+  user { $::vault::user:
+    ensure => present,   
+  }
+  Group[$vault::group] -> User[$vault::user]
+  group { $::vault::group:
+    ensure => present,
+  }  
 }
+
