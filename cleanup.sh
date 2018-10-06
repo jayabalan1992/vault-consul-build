98a14fe841c944153f945a0e218fc9068dbe9002
jayabalan <jayabalanbala@gmail.com>
2018-10-06 19:22:44 -0400

    Added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#This script cleanup the vault and consul installation
+#!/bin/bash
+rm -rf /usr/local/bin/*
+rm -rf /etc/systemd/system/
+rm -rf /etc/systemd/system/vault.service
+rm -rf /etc/systemd/system/consul.service
+rm -rf /usr/local/etc/*
+rm -rf /etc/vault.d/
+rm -rf /etc/consul.d/
+
